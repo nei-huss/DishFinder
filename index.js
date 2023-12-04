127a9a6c10382f6cfd1b5f533a5844451588acce
Neima <huss.nei@berkeley.edu>
2023-12-04 15:36:23 -0800

    neima

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Your JavaScript code goes here
 });
+
+const searchButton = document.getElementById("search button");
+const mealList= document.getElementById("meal");
+
