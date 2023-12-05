69ce6b7c5cbee4d2e157973c9231c3634634f442
Neima <huss.nei@berkeley.edu>
2023-12-04 16:52:19 -0800

    neima

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 const searchButton = document.getElementById("search-button");
-const dishList= document.getElementById("Dish");
-const recipeCloseButton = document.getElementById("Recipe-Close-Button");
-const dishDetails = document.querySelector("Dish-Content");
+const dishList= document.getElementById("dish");
+const dishDetails = document.querySelector(".dish-content");
+
+searchButton.addEventListener("click", getDishlist);
+dishList.addEventListener("click", getDishRecipe);
+
+function getDishList () {
+    let searchText = document.getElementById("search-input").value.trim();
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => console.error('Error:', error));
+}
