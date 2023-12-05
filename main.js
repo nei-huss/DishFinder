bf20da135daff17ae0779292d7b7fef2b01844d1
Neima <huss.nei@berkeley.edu>
2023-12-04 19:06:06 -0800

    neima

--- main.js
+++ main.js
@@ -2,16 +2,33 @@ const searchButton = document.getElementById("search-button");
 const dishList = document.getElementById("dish");
 const dishDetails = document.querySelector(".dish-content");
 
-searchButton.addEventListener("click", getDishlist);
-dishList.addEventListener("click", getDishRecipe);
+searchButton.addEventListener("click", getdishList);
 
-function getDishList () {
+function getdishList () {
     let searchText = document.getElementById("search-input").value.trim();
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`)
+    fetch(`www.themealdb.com/api/json/v1/1/filter.php?i=${searchText}`)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
-        })
-        .catch(error => console.error('Error:', error));
+            let html = ""; 
+            if(data.meals){
+                data.meals.forEach(meal => {
+                    html += `
+                    <div class = "meal-item" data-id = "${meal.idMeal}">
+                    <div class = "meal-img" >
+                    <img src = "${meal.strMealThumb}" alt = "food">
+                    </div>
+                <div class = "meal-name" >
+                    <h3>${meal.strMeal}</h3>
+                    <a href = "#" class ="recipe-button">Recipe</a>
+                </div>
+            < div>     
+                    `;
+
+                });
+            }
+
+            dishList.innerHTML = html;
+
+        })     
 }
 
