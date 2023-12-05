f1f84f68645d146619c4891e5964cb30d749ae7c
Neima <huss.nei@berkeley.edu>
2023-12-04 16:24:14 -0800

    neima

--- index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Your JavaScript code goes here
-});
-
-const searchButton = document.getElementById("search button");
-const mealList= document.getElementById("meal");
-
---
+++ main.js
@@ -0,0 +1,4 @@
+const searchButton = document.getElementById("search-button");
+const dishList= document.getElementById("Dish");
+const recipeCloseButton = document.getElementById("Recipe-Close-Button");
+const dishDetails = document.querySelector("Dish-Content");
